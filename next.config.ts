3c7d879d7cdc716a80d888cc1ad288d00e2ea54b
nahalfirouzi <nahalfirouzi@gmail.com>
2026-02-22 12:45:27 +1100

    Update next.config.ts to configure output, basePath, and image optimization settings

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+
+  /* subdir path for github pages */
+  basePath: "/dbas",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
